5c6cfdb2df926bd8443d1db1195fa055573e1293
vlad.apostol <vlad.apostol@nvtech.ro>
2021-02-02 17:59:45 +0200

    functionality to add text

--- exporter.js
+++ exporter.js
@@ -2,15 +2,16 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 export default class Exporter {
-  constructor(charts) {
+  constructor(charts, texts = null) {
     this.charts = charts;
     this.doc = new jsPDF("landscape", "px", "letter");
+    this.texts = texts;
   }
   export_pdf() {
     return new Promise((resolve, reject) => {
       try {
         this.charts.forEach(async (chart, index, charts) => {
-          this.doc = await this.create_page(chart, this.doc);
+          this.doc = await this.create_page(chart, this.doc, texts[index]);
           if (index === charts.length - 1) {
             resolve(this.doc); // this.doc.save("charts.pdf"); // <-- at the end of the loop; return PDF
             return;
@@ -23,7 +24,7 @@ export default class Exporter {
     });
   }
 
-  async create_page(chart, doc) {
+  async create_page(chart, doc, text) {
     const canvas = await html2canvas(chart, {
       scrollY: -window.scrollY,
       scale: 5 // <-- this is to increase the quality. don't raise this past 5 as it doesn't get much better and just takes longer to process
@@ -37,6 +38,8 @@ export default class Exporter {
       marginX = (pageWidth - canvasWidth) / 2,
       marginY = (pageHeight - canvasHeight) / 2;
 
+    doc.setFontSize(16);
+    doc.text(20, 30, text);
     doc.addImage(image, "JPEG", marginX, marginY, canvasWidth, canvasHeight);
 
     return doc;
